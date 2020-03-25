bc1904009bfaef1a3b33769ab72b91a9470a3a23
jamesyaputra <jamesyaputra1704@gmail.com>
2020-03-25 11:52:43 +0800

    Basic navbar completed

---
+++ src/modules/navbar/Navbar.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { Button, Menu, Search } from 'semantic-ui-react';
+import styles from './Navbar.module.css';
+
+export class Navbar extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu borderless size='large'>
+        <Menu.Item
+          name='Home'
+          active={activeItem === 'Home'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          <img style={{width: 70}} src='https://f.seedly.sg/logo/seedly-logo-blue.png' />
+        </Menu.Item>
+        
+        <Menu.Item
+          name='banking'
+          active={activeItem === 'banking'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Banking
+        </Menu.Item>
+
+        <Menu.Item
+          name='cards'
+          active={activeItem === 'cards'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Cards
+        </Menu.Item>
+
+        <Menu.Item
+          name='investments'
+          active={activeItem === 'investments'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Investments
+        </Menu.Item>
+
+        <Menu.Item
+          name='utilitiesAndBills'
+          active={activeItem === 'utilitiesAndBills'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Utilities & Bills
+        </Menu.Item>
+
+        <Menu.Item
+          name='insurance'
+          active={activeItem === 'insurance'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Insurance
+        </Menu.Item>
+
+        <Menu.Item
+          name='community'
+          active={activeItem === 'community'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Community
+        </Menu.Item>
+
+        <Menu.Item
+          name='content'
+          active={activeItem === 'content'}
+          color='blue'
+          onClick={this.handleItemClick}
+        >
+          Content
+        </Menu.Item>
+
+        <Menu.Menu position='right'>
+          <Search
+            className={styles.search}
+            size='small'
+            placeholder='Search for anything'
+          />
+
+          <Menu.Item>
+            <Button color='blue'>Join</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
+
+export default Navbar;
