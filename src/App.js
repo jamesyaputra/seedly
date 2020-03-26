b55ec59458f5d2dc9fc45b7f145a4eaac9f853ac
jamesyaputra <jamesyaputra1704@gmail.com>
2020-03-26 15:51:31 +0800

    Installed redux thunk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navbar } from './modules/navbar/Navbar';
 import { Questions } from './modules/questions/Questions';
-import './App.css';
 
 function App() {
   return (
